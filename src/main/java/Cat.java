176165530b979078b8aa3a84389f0a63ed70912e
Bakulin Vladimir <wal1984@mail.ru>
2023-02-05 16:49:06 +0700

    first commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,20 @@
+public class Cat {
+    private String nameCat;
+    private byte age;
+
+    public String getNameCat() {
+        return nameCat;
+    }
+
+    public void setNameCat(String nameCat) {
+        this.nameCat = nameCat;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+}

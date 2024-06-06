20d5417220ef8f5af2e53121ee7d3ba317fc06f1
Bakulin Vladimir <wal1984@mail.ru>
2024-06-06 15:16:37 +0700

    good result

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,10 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 
 public class App {
     public static void main(String[] args) {
+
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
 
